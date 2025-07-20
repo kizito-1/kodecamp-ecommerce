bdb550012603792cbacbff31cd44f1e573746dd6
kizito-1 <kizitochidubem01@gmail.com>
2025-07-20 21:56:58 +0100

    working on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
 import "./App.css";
+import Home from "./Pages/Home.jsx";
+import Login from "./Pages/Login.jsx";
+import Signup from "./Pages/Signup.jsx";
+import UserNavbar from "./components/UserNavbar.jsx";
+import GuestNavbar from "./components/GuestNavbar.jsx";
+import ProductDetail from "./Pages/ProductDetail.jsx";
+import Wishlist from "./Pages/Wishlist.jsx";
+import { useAuth } from "./Context/AuthContext.jsx";
+import Cart from "./Pages/Cart.jsx";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./Context/ProtectedRoute.jsx";
 
 function App() {
-  return <></>;
+  const { user } = useAuth();
+
+  return (
+    <>
+      {user ? <UserNavbar /> : <GuestNavbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/product/:id"
+          element={
+            <ProtectedRoute>
+              <ProductDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/wishlist"
+          element={
+            <ProtectedRoute>
+              <Wishlist />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/config.js";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext);
